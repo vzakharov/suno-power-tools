e35ddd41659863f5aabf9f74006e572ca5fc9a6c
Vova Zakharov <vzakharov@gmail.com>
2025-01-07 13:47:10 +0300

    triple underscore in templating, just in case someone uses a dunder

--- src/templating.ts
+++ src/templating.ts
@@ -1,11 +1,11 @@
 import { StringKey, UnionToIntersection } from "./types";
 
-export type MonoTemplate<T extends string> = `${string}__${T}__${string}`;
+export type MonoTemplate<T extends string> = `${string}___${T}___${string}`;
 
 export type Template<T extends string> = UnionToIntersection<MonoTemplate<T>>;
 
 export function renderTemplate<T extends Record<string, string>>(template: Template<StringKey<T>>, values: T) {
   return Object.keys(values).reduce((acc, key) => {
-    return acc.replace(`__${key}__`, values[key]);
+    return acc.replace(`___${key}___`, values[key]);
   }, template as string);
 };
