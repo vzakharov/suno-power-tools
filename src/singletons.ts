88e952b93071ed94d9434adc081b1afdb62d6f37
Vova Zakharov <vzakharov@gmail.com>
2025-01-28 11:58:06 +0300

    singleton factory

--- src/singletons.ts
+++ src/singletons.ts
@@ -1,3 +1,5 @@
+import { div } from "./smork/dom";
+import { Func } from "./types";
 import { getOrSet, mutated } from "./utils";
 
 const SINGLETON_MAP = new WeakMap<WeakKey, any>();
@@ -22,3 +24,26 @@ export const Singleton = mutated(
 );
 
 export type Singleton<T> = ReturnType<typeof createSingleton<T>>;
+
+export function SingletonFactory<
+  TParams extends object[],
+  TInstance
+>(Factory: Func<TParams, TInstance>) {
+  return (...args: TParams) => Singleton.by(...args)(() => Factory(...args))
+};
+export type SingletonFactory<TParams extends object[], TInstance> = ReturnType<typeof SingletonFactory<TParams, TInstance>>;
+
+// e.g.
+type Product = { name: string; price: number };
+type User = { name: string };
+const ProductCard = SingletonFactory(
+  (product: Product, user: User) => div(
+    { class: 'product-card' }, [
+      div({ class: 'product-card__name' }, [ product.name ]),
+      div({ class: 'product-card__price' }, [ String(product.price) ]),
+      div({ class: 'product-card__user' }, [ user.name ]),
+    ]
+  )
+)
+// I.e., if ProductCard is called with the same product and user, it will return the same element instead of creating a new one
+// (This won't exactly work because it won't be reactive, but this is just an illustration of the concept)
