2ed500572e0ba43e0e28d2a6a290d93964eed628
Vova Zakharov <vzakharov@gmail.com>
2025-01-30 13:11:41 +0300

    delete old

--- src/smork/newRefs.ts
+++ src/smork/newRefs.ts
@@ -1,93 +1,193 @@
-// import { watch } from "fs";
-// import { Func } from "../types";
-// import { MetaRegister, Register } from "../utils";
+import { maxOf } from "../lodashish";
+import { NOT_SET, NotSet } from "../types";
+import { Box, inc, Metabox } from "../utils";
 
-// const RefCore = MetaRegister();
-// type RefCore<T = any> = ReturnType<typeof RefCore<T>>;
 
-// // const watchers: 
-// //     ( <T>(ref: RefCore<T>) => Watchers<T> )
-// //   & ( <T>(ref: RefCore<T>, value: Watchers<T>) => void )
-// //  = Register(RefCore, () => new Set<Func<[any, any], void>>());
-// function watchers<T>(ref: RefCore<T>, setValue?: Set<Watcher<T>>) {
-//   return Register(RefCore, () => new Set<Watcher<T>>())(ref, setValue);
-// };
+const lastMaxRootIteration = Metabox((ref: ComputedRef<any>) => 0);
+const sourceRoots = Metabox((ref: ComputedRef<any>) => new Set<RootRef<any>>())
+let maxIteration = 0;
+const iteration = Metabox((root: RootRef<any>) => maxIteration++);
+const computees = new Set<ComputedRef<any>>();
 
-// type Watcher<T> = Func<[now: T, before: T], void>;
+export function RootRef<T>(value: T) {
 
-// function activeWatchers<T>(ref: RefCore<T>) {
-//   return Register(RefCore, () => new WeakSet<Func<[T, T], void>>())(ref);
-// };
+  const self = Box(
+    () => {
+      computees.forEach(computee => {
+        sourceRoots(computee).add(self);
+        if ( lastMaxRootIteration(computee) < iteration(self) ) {
+          lastMaxRootIteration(computee, iteration(self));
+        };
+      });
+      return value
+    },
+    setValue => {
+      value = setValue;
+      iteration(self, inc);
+    },
+  );
 
-// const targets = Register(RefCore, () => new Set<RefCore>());
+  return self;
+};
 
-// function setRef<T>(ref: RefCore<T>, value: T) {
-//   const oldValue = ref();
-//   if ( value !== oldValue ) {
-//     ref(value);
-//     tarnishTargets(ref);
-//     try {
-//       for ( const watcher of watchers(ref) ) {
-//         if ( activeWatchers(ref).has(watcher) ) {
-//           console.warn('smork: watcher is already active — perhaps a circular dependency — exiting watch to prevent infinite loop');
-//           break;
-//         };
-//         activeWatchers(ref).add(watcher);
-//         watcher(value, oldValue);
+export type RootRef<T> = ReturnType<typeof RootRef<T>>;
+
+export function ComputedRef<T>(getter: () => T) {
+
+  let cachedValue = NotSet<T>();
+
+  const self = Box(
+    () => {
+      if ( 
+        cachedValue === NOT_SET
+      ) {
+        return recompute()
+      } else {
+        const maxRootIteration = maxOf(sourceRoots(self), iteration);
+        if ( maxRootIteration > lastMaxRootIteration(self) ) {
+          lastMaxRootIteration(self, maxRootIteration);
+          return recompute();
+        };
+        return cachedValue;
+      };
+
+      function recompute() {
+        sourceRoots(self).clear();
+        computees.add(self);
+        try {
+          return cachedValue = getter();
+        } finally {
+          computees.delete(self);
+        };
+      };
+    }
+  );
+
+  return self;
+};
+
+export type ComputedRef<T> = ReturnType<typeof ComputedRef<T>>;
+
+// const [ tarnish, clean ] = [ +1, -1 ].map(
+//   increment => ( target: Target ) => (
+//     function handle(target: Target, handled = new WeakSet<Target>()) {
+//       const [ targetRef, iteration ] = target;
+//       const targetData = metadata(targetRef);
+//       if ( targetData.iteration === iteration ) {
+//         targetData.dirtiness += increment;
+//         targetData.targets.forEach(target => {
+//           if (!handled.has(target)) { // to prevent infinite loops for circularly dependent refs
+//             handled.add(target);
+//             handle(target, handled);
+//           };
+//         });
+//       } else {
+//         targetData.targets.delete(target);
 //       };
-//     } finally {
-//       activeWatchers(ref, new WeakSet());
-//     };
+//     }
+//   )(target)
+// );
+
+// export function ref<T>(value: NonFunction<T>): Ref<T, { writable: true }>;
+// export function ref<T>(getter: () => T): Ref<T, { writable: false }>;
+// export function ref<T>(getter: () => T, setter: (value: T) => void): Ref<T, { writable: true }>;
+// export function ref<T>(getterOrValue: T | (() => T), setter?: (value: T) => void) {
+//   return Ref(isFunction(getterOrValue) ? getterOrValue : () => getterOrValue, setter);
+// };
+
+// export class ComputeeStackMismatchError extends Error {
+//   constructor(stack: Target[], expected: Target) {
+//     super(`Computee stack mismatch. Expected ${metadata(expected[0]).id} but got ${stack.map(([ref]) => metadata(ref).id).join(', ')}`);
 //   };
 // };
 
-// const dirty = new WeakSet<RefCore>();
+// export function Ref<T>(getter: () => T, setter: Undefinable<(value: T) => void>) {
 
-// function tarnishTargets(ref: RefCore) {
-//   targets(ref).forEach(tarnish);
-// };
+//   let cachedValue = NotSet<T>();
 
-// function tarnish(ref: RefCore) {
-//   if ( dirty.has(ref) ) return;
-//   dirty.add(ref);
-//   if ( watchers(ref).size ) {
-//     recalculate(ref);
-//   } else {
-//     tarnishTargets(ref);
-//   };
-// };
+//   const self = <Ref<T, RefOptions>>((
+//     value?: Defined<T>,
+//   ) => {
 
+//     const data = metadata(self);
+//     const { targets } = data;
 
-// export function Ref<T>(initialValue: T) {
+//     if ( value === undefined ) {
 
-//   const core = RefCore(initialValue);
+//       if ( cachedValue === NOT_SET || data.dirtiness ) {
 
-//   // const ref = () => (
-//   //   currentComputedTracker?.(ref),
-//   //   core()
-//   // );
+//         const target = computeeStack.at(-1);
+//         target && targets.add(target);
 
-//   function ref(): T;
-//   function ref(value: T): void;
-//   function ref<U>(mapper: Func<[now: T, before: T], U>): MappedRef<U>;
-//   function ref<U>(arg?: T | Func<[now: T, before: T], U>) {
-//     if ( arguments.length === 0 ) {
-//       return core();
-//     } else if ( typeof arg === 'function' ) {
-//       return MappedRef(core, arg);
+//         const selfTarget = [ self, data.iteration++ ] as const;
+//         computeeStack.push(selfTarget);
+//         try {
+//           const newValue = getter();
+//           if ( newValue === cachedValue ) {
+//             targets.forEach(clean); // i.e. this value isn't changed, no need to recompute the target at least as far as this source is concerned
+//             data.alreadyTarnished = false;
+//           } else {
+//             cachedValue = newValue;
+//           };
+//           data.dirtiness = 0;
+//         } finally {
+//           if ( computeeStack.pop() !== selfTarget ) {
+//             throw new ComputeeStackMismatchError(computeeStack, selfTarget);
+//           };
+//         };
+
+//       };
+
+//       return cachedValue;
+
 //     } else {
-//       setRef(core, arg);
+//       if ( setter === undefined ) {
+//         throw NonWritableRefError();
+//       };
+//       setter(value);
+//       if ( !data.alreadyTarnished ) {
+//         data.alreadyTarnished = true;
+//         targets.forEach(tarnish);
+//       };
+//       return value;
 //     };
-//   };
 
-//   // tba
+//   });
 
-//   return ref;
+//   return self;
 
 // };
 
-// export type Ref<T> = ReturnType<typeof Ref<T>>;
+// // some tests
 
-// const name = RefCore('John Doe');
+// const number = Ref(3)
+// // Inferred:
+// // const number: Ref<number, {
+// //   writable: true;
+// // }>
+// console.log(number()); // 3
+// number(4);
+// console.log(number()); // 4
 
-// const nameWatchers = watchers(name, new Set<Func<[string], void>>())()
+// const string = number(String);
+// // const string: Ref<string, {
+// //   writable: false;
+// // }>
+// console.log(string()); // "4"
+// try {
+//   // @ts-expect-error
+//   string("5");
+// } catch (e) { console.log(e.message) } // Cannot write to a non-writable ref
+
+// const twoWayString = number(String, Number);
+// // const twoWayString: Ref<string, {
+// //   writable: true;
+// // }>
+// twoWayString("5"); // Ok because we can write to it
+// console.log(twoWayString()); // "5"
+
+// // additional bonus
+// const boolean = Ref(false);
+// if ( !boolean ) { // <-- easily seen as a mistake because it's highlighted as a function
+//   console.log("You forgot to call the ref");
+// }
